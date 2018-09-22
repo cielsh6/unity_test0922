0cffbf0b8bd1ab196eadf819fe91f5c6b5c8e265
ahnyuchan <ultrauc123@naver.com>
2018-09-22 13:29:40 +0900

    unity test

---
+++ TEST_RUN_GAME/Assets/Script/BlockCreater.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCreater : MonoBehaviour {
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if(_createInterval <= _createDuration)
+		{
+			_createDuration = 0.0f;
+			CreateBlock();
+		}
+		_createDuration += Time.deltaTime;
+	}
+
+	float _createInterval = 2.0f;
+	float _createDuration = 0.0f;
+
+	public GameObject BlockPrefab;
+	public float BlockSpeed = 5.0f;
+	
+	void CreateBlock()
+	{
+		//프리팹을 이용해서 블럭 게임오브젝트 생성
+		GameObject Block = GameObject.Instantiate(BlockPrefab);
+
+		//블럭의 위치를 지정
+		Block.transform.position = transform.position;
+
+		//블럭의 속도 적용
+		Block.GetComponent<Block>().SetSpeed(BlockSpeed);
+
+		//만들어진 블럭 파괴
+		GameObject.Destroy(Block, 6.0f);
+	}
+}
---
+++ TEST_RUN_GAME/Assets/Script/PlayerCharacter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	void Start()
+	{
+		ChangeState(eState.IDLE);
+	}
+
+	void Update()
+	{
+
+	}
+
+	public enum eState
+	{
+		IDLE,
+		RUN,
+	}
+
+	void ChangeState(eState state)
+	{
+		switch(state)
+		{
+			case eState.IDLE:
+				GetAnimator().SetTrigger("Idle");
+				break;
+			case eState.RUN:
+				GetAnimator().SetTrigger("Run");
+				break;
+
+		}
+	}
+
+	Animator GetAnimator()
+	{
+		return gameObject.GetComponent<Animator>();
+	}
+}
